ed4d1d6699d142af713b68fb674e1427af2e9c80
Ramadhan Wahyu <ramadhanwahyu24@gmail.com>
2026-07-27 18:08:14 +0700

    chore: remove unused service

--- src/filters/filters.module.ts
+++ src/filters/filters.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { FiltersService } from './filters.service';
 import { FiltersController } from './filters.controller';
 import { CategoriesModule } from '../categories/categories.module';
 
 @Module({
   controllers: [FiltersController],
-  providers: [FiltersService],
   imports: [CategoriesModule],
 })
 export class FiltersModule {}
